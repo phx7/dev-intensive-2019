2995955a37bab9f97f343a37605a8535a8f3303d
a.zhukovskiy <a.zhukovskiy@crpo.su>
2019-07-16 13:18:54 +0300

    humanize

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,8 +1,8 @@
 package ru.skillbranch.devintensive.extensions
 
-import java.sql.Time
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
@@ -27,16 +27,24 @@ fun Date.add(value: Int, unit: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    return when (val diff = (date.time - this.time) / 1000) {
-        in 0..1 -> "только что"
-        in 1..45 -> "несколько секунд назад"
-        in 45..75 -> "минуту назад"
-        in 75..45 * 60 -> "${diff / 60} минут назад"
-        in 45 * 60..75 * 60 -> "час назад"
-        in 75 * 60..22 * 3600 -> "${diff / 3600} часов назад"
-        in 22 * 3600..26 * 3600 -> "день назад"
-        in 26 * 3600..360 * 24 * 3600 -> "${diff / (24 * 3600)} дней назад"
-        else -> "более года назад"
+    return when (val diff : Long = (date.time - this.time)) {
+        in -1 * SECOND..1 * SECOND -> "только что"
+        in 1 * SECOND..45 * SECOND -> "несколько секунд назад"
+        in -45 * SECOND..-1 * SECOND -> "через несколько секунд"
+        in 45 * SECOND..75 * SECOND -> "минуту назад"
+        in -75 * SECOND..-45 * SECOND -> "через минуту"
+        in 75 * SECOND..45 * MINUTE -> "${TimeUnits.MINUTE.plural(diff / MINUTE)} назад"
+        in -45 * MINUTE .. -75 * SECOND -> "через ${TimeUnits.MINUTE.plural(abs(diff) / MINUTE)}"
+        in 45 * MINUTE..75 * MINUTE -> "час назад"
+        in -75 * MINUTE .. -45 * MINUTE -> "через час"
+        in 75 * MINUTE..22 * HOUR -> "${TimeUnits.HOUR.plural(diff / HOUR)} назад"
+        in -22 * HOUR..-75 * MINUTE -> "через ${TimeUnits.HOUR.plural(abs(diff) / HOUR)}"
+        in 22 * HOUR..26 * HOUR -> "день назад"
+        in -26 * HOUR..-22 * HOUR -> "через день"
+        in 26 * HOUR..360 * DAY -> "${TimeUnits.DAY.plural(diff / DAY)} назад"
+        in -360 * DAY..-26 * HOUR -> "через ${TimeUnits.DAY.plural(abs(diff) / DAY)}"
+        in 360 * DAY .. Long.MAX_VALUE -> "более года назад"
+        else -> "более чем через год"
     }
 }
 
@@ -46,17 +54,17 @@ enum class TimeUnits {
     HOUR,
     DAY;
 
-    fun plural(value: Int): String {
+    fun plural(value: Long): String {
         var str = ""
-        val time_single: Int = value % 10
-        val time_double: Int = value % 100
+        val time_single: Long = value % 10
+        val time_double: Long = value % 100
         when (this) {
             SECOND -> {
                 str = when (time_double) {
                     in 10..19 -> "секунд"
                     else -> when (time_single) {
-                        0, 5, 6, 7, 8, 9 -> "секунд"
-                        1 -> "секунду"
+                        0L, 5L, 6L, 7L, 8L, 9L -> "секунд"
+                        1L -> "секунду"
                         in 2..4 -> "секунды"
                         else -> "секунд"
                     }
@@ -66,8 +74,8 @@ enum class TimeUnits {
                 str = when (time_double) {
                     in 10..19 -> "минут"
                     else -> when (time_single) {
-                        0, 5, 6, 7, 8, 9 -> "минут"
-                        1 -> "минуту"
+                        0L, 5L, 6L, 7L, 8L, 9L -> "минут"
+                        1L -> "минуту"
                         in 2..4 -> "минуты"
                         else -> "минут"
                     }
@@ -77,8 +85,8 @@ enum class TimeUnits {
                 str = when (time_double) {
                     in 10..19 -> "часов"
                     else -> when (time_single) {
-                        0, 5, 6, 7, 8, 9 -> "часов"
-                        1 -> "час"
+                        0L, 5L, 6L, 7L, 8L, 9L -> "часов"
+                        1L -> "час"
                         in 2..4 -> "часа"
                         else -> "часов"
                     }
@@ -89,8 +97,8 @@ enum class TimeUnits {
                 str = when (time_double) {
                     in 10..19 -> "дней"
                     else -> when (time_single) {
-                        0, 5, 6, 7, 8, 9 -> "дней"
-                        1 -> "день"
+                        0L, 5L, 6L, 7L, 8L, 9L -> "дней"
+                        1L -> "день"
                         in 2..4 -> "дня"
                         else -> "дней"
                     }
