6d66d995fdfc8229fa0e9cc9090214a71e5d2fc9
yolandabecool <phx468@gmail.com>
2019-07-21 01:05:05 +0300

    fix

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -11,5 +11,5 @@ class TextMessage(
     date: Date = Date(),
     var text: String
 ): BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage(): String = "${from?.firstName} ${if(isIncoming) "получил" else "отправил"} изображение $text ${date.humanizeDiff()}}"
+    override fun formatMessage(): String = "${from?.firstName} ${if(isIncoming) "получил" else "отправил"} изображение \"$text\" ${date.humanizeDiff()}}"
 }
